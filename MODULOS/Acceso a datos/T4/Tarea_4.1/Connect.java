665b515f472b60826d31e6cfee55f09792d26d72
Ana Pastor <146741422+Anaroncero@users.noreply.github.com>
2024-10-19 13:55:23 +0200

    feat: tarea 4.1 acceso a datos

--- MODULOS/Acceso a datos/T4/Tarea_4.1/Connect.java
+++ MODULOS/Acceso a datos/T4/Tarea_4.1/Connect.java
@@ -1,36 +1,164 @@
 import java.sql.Connection;
-import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Connect {
 
-    private static final String driver = "com.mysql.cj.jbc.Driver";
-    private static final String url_conexion = "jdbc:mysql://localhost:3306/prueba";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL_CONEXION = "jdbc:mysql://localhost:3306/taquilla";
 
     public static void main(String[] args) {
-        
-        //Conectamos a la bbdd
-        final String usuario = "root";
-        final String password = "Med@c";
+
+        // Variables para la conexión a la BDD
+        final String USUARIO = "root";
+        final String PASSWORD = "12345";
         Connection dbConnection = null;
-        Statement statement = null;
+
 
         try {
+            // Crear menú
+            System.out.println(
+                    "Menú de consultas:\n Opción 1: Mostrar todas las películas\n Opción 2: Mostrar todas las películas de un actor por su nombre\n Opción 3: Mostrar todas las películas, sus actores y sus directores\n Opción 4: Mostrar la suma de películas por nombre de actor\n Opción 5: Salir");
+            Scanner sc = new Scanner(System.in);
+            int opcion;
 
-            ClassforName(driver);
-            dbConnection = DriverManager.getConnection(url_conexion, usuario, password);
-            String consulta = "SELECT p.titulo AS Titulo_Pelicula, a.nombre AS Nombre_Actor, a.apellidos AS Apellidos_Actor\n" + //
-                                "FROM peliculas p\n" + //
-                                "JOIN peliculas_actores pa ON p.id = pa.id_pelicula\n" + //
-                                "JOIN actores a ON pa.id_actor = a.id;";
-            statement = dbConnection.createStatement();
-            ResulSet rs = statement.excuteQuery(consulta);
+            // Carga el controlador de la base de datos
+            Class.forName(DRIVER);
+            // Establecer conexión a la base de datos
+            dbConnection = DriverManager.getConnection(URL_CONEXION, USUARIO, PASSWORD);
 
-        } catch (Exception e) {
-            
+            // Bucle del menú con las opciones y la llamada a los métodos
+            do {
+                System.out.println("Introduzca una opción del menú (1-5): ");
+                opcion = sc.nextInt();
+                sc.nextLine(); // Quitar el salto de línea
+
+                switch (opcion) {
+                    case 1:
+                        mostrarPeliculas(dbConnection); // pasamos la conexión como parámetro 
+                        break;
+
+                    case 2:
+                        mostrarPeliculasPorActor(dbConnection, sc);
+                        break;
+
+                    case 3:
+                        mostrarPeliculasActoresDirectores(dbConnection);
+                        break;
+
+                    case 4:
+                        contarPeliculasPorActor(dbConnection, sc);
+                        break;
+
+                    case 5:
+                        System.out.println("Ha elegido salir del menú");
+                        break;
+
+                    default:
+                        System.out.println("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+
+            } while (opcion != 5);
+
+            sc.close();
+
+        } catch (SQLException e) {
+            System.out.println("Error de SQL: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Controlador no encontrado: " + e.getMessage());
+        } finally {
+            try {
+                if (dbConnection != null) {
+                    dbConnection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error cerrando la conexión: " + e.getMessage());
+            }
         }
+    }
 
+
+    // Métodos
+    private static void mostrarPeliculas(Connection dbConnection) throws SQLException { // Pasamos el parametro Connection dbConnection para pasar la conexión a la bdd al método, asi no tenemos que crear conexión de nuevo dentro del método 
+        String mostrarPeliculas = "SELECT * FROM peliculas";
+        try (PreparedStatement statement = dbConnection.prepareStatement(mostrarPeliculas);
+             ResultSet rs = statement.executeQuery()) {
+
+            while (rs.next()) {
+                System.out.println("------------------------------------");
+                System.out.println("id: " + rs.getString("id"));
+                System.out.println("Nombre: " + rs.getString("nombre"));
+                System.out.println("Año: " + rs.getString("anio"));
+            }
+        }
+    }
+
+    private static void mostrarPeliculasPorActor(Connection dbConnection, Scanner sc) throws SQLException { // pasamos como parámetro el scanner para que el método lea la entrada del usuario sin tener que crear una nueva dentro del metodo
+        System.out.println("Introduzca el nombre del actor: ");
+        String nombreActorInput = sc.nextLine();
+        String peliculasActor = "SELECT p.nombre AS pelicula FROM peliculas p " +
+                                "JOIN peliculas_actores pa ON p.id = pa.id_pelicula " +
+                                "JOIN actores a ON pa.id_actor = a.id WHERE a.nombre = ?";
+
+        try (PreparedStatement preparedStatement = dbConnection.prepareStatement(peliculasActor)) {
+            preparedStatement.setString(1, nombreActorInput);
+            try (ResultSet rs1 = preparedStatement.executeQuery()) {
+                boolean hayResultado = false;
+
+                while (rs1.next()) {
+                    hayResultado = true;
+                    System.out.println(rs1.getString("pelicula"));
+                }
+
+                if (!hayResultado) {
+                    System.out.println("No se encontraron películas para el actor: " + nombreActorInput);
+                }
+            }
+        }
+    }
+
+    private static void mostrarPeliculasActoresDirectores(Connection dbConnection) throws SQLException {
+        String actorPeliculaDirector = "SELECT d.nombre AS director, p.nombre AS pelicula, " +
+                                       "a.nombre AS actor_nombre, a.apellido AS actor_apellido " +
+                                       "FROM peliculas p " +
+                                       "JOIN directores_peliculas dp ON p.id = dp.id_pelicula " +
+                                       "JOIN directores d ON dp.id_director = d.id " +
+                                       "JOIN peliculas_actores pa ON p.id = pa.id_pelicula " +
+                                       "JOIN actores a ON pa.id_actor = a.id";
+
+        try (PreparedStatement statement = dbConnection.prepareStatement(actorPeliculaDirector);
+             ResultSet rs2 = statement.executeQuery()) {
+
+            while (rs2.next()) {
+                System.out.println("Director: " + rs2.getString("director") + 
+                                   ", Película: " + rs2.getString("pelicula") + 
+                                   ", Actor: " + rs2.getString("actor_nombre") + " " + 
+                                   rs2.getString("actor_apellido"));
+            }
+        }
+    }
+
+    private static void contarPeliculasPorActor(Connection dbConnection, Scanner sc) throws SQLException {
+        System.out.println("Introduzca el nombre del actor: ");
+        String nomActorInput = sc.nextLine();
+        String peliculasActorCount = "SELECT COUNT(p.id) AS cantidad_peliculas FROM peliculas p " +
+                                     "JOIN peliculas_actores pa ON p.id = pa.id_pelicula " +
+                                     "JOIN actores a ON pa.id_actor = a.id WHERE a.nombre = ?";
+
+        try (PreparedStatement preparedStatement = dbConnection.prepareStatement(peliculasActorCount)) {
+            preparedStatement.setString(1, nomActorInput);
+            try (ResultSet rs3 = preparedStatement.executeQuery()) {
+                if (rs3.next()) {
+                    int cantidadPeliculas = rs3.getInt("cantidad_peliculas");
+                    System.out.println("Cantidad de películas del actor " + nomActorInput + ": " + cantidadPeliculas);
+                } else {
+                    System.out.println("No se encontraron resultados.");
+                }
+            }
+        }
     }
 }
---
+++ MODULOS/Acceso a datos/T4/Tarea_4.1/sql/taquilla.sql
@@ -0,0 +1,165 @@
+DROP DATABASE IF EXISTS taquilla;
+CREATE DATABASE taquilla;
+USE taquilla;
+
+CREATE TABLE peliculas (
+	id int auto_increment primary key,
+	nombre varchar(100) not null,
+    anio int not null
+);
+
+CREATE TABLE actores (
+	id int auto_increment primary key,
+    nombre varchar(50) not null,
+    apellido varchar(50) not null
+);
+
+CREATE TABLE directores (
+	id int auto_increment primary key,
+    nombre varchar(50) not null
+);
+
+CREATE TABLE peliculas_actores (
+	id_pelicula int,
+    id_actor int,
+    
+    PRIMARY KEY(id_pelicula, id_actor),
+    FOREIGN KEY (id_pelicula) REFERENCES peliculas(id),
+    FOREIGN KEY (id_actor) REFERENCES actores(id)
+);
+
+CREATE TABLE directores_peliculas (
+	id_director int,
+    id_pelicula int,
+    
+    PRIMARY KEY(id_director, id_pelicula),
+    FOREIGN KEY (id_director) REFERENCES directores(id),
+    FOREIGN KEY (id_pelicula) REFERENCES peliculas(id)
+);
+
+-- Registros peliculas
+INSERT peliculas(nombre, anio) VALUES("Titanic", 1997);
+INSERT peliculas(nombre, anio) VALUES("La maldición de la Perla Negra", 2003);
+INSERT peliculas(nombre, anio) VALUES("Piratas del Caribe: La venganza de Salazar", 2017);
+INSERT peliculas(nombre, anio) VALUES("Fast & Furious", 2001);
+INSERT peliculas(nombre, anio) VALUES("Fast X", 2003);
+INSERT peliculas(nombre, anio) VALUES("Crepúsculo", 2008);
+INSERT peliculas(nombre, anio) VALUES("La Saga Crepúsculo: Amanecer - Parte 2", 2012);
+INSERT peliculas(nombre, anio) VALUES("Iron Man 3", 2013);
+INSERT peliculas(nombre, anio) VALUES("Capitán América: El Soldado de Invierno", 2014);
+INSERT peliculas(nombre, anio) VALUES("Deadpool", 2016);
+INSERT peliculas(nombre, anio) VALUES("Deadpool 3", 2024);
+INSERT peliculas(nombre, anio) VALUES("Top Gun", 1986);
+INSERT peliculas(nombre, anio) VALUES("Misión: Imposible", 1996);
+INSERT peliculas(nombre, anio) VALUES("Misión: Imposible - Fallout", 2018);
+
+-- Actores
+INSERT actores(nombre, apellido) VALUES("Leonardo", "DiCaprio");
+INSERT actores(nombre, apellido) VALUES("Kate", "Windslet");
+INSERT actores(nombre, apellido) VALUES("Johnny Depp", "Depp");
+INSERT actores(nombre, apellido) VALUES("Orlando", " Bloom");
+INSERT actores(nombre, apellido) VALUES("Vin", "Diesel");
+INSERT actores(nombre, apellido) VALUES("Paul", "Walker");
+INSERT actores(nombre, apellido) VALUES("Kristen", "Stewart");
+INSERT actores(nombre, apellido) VALUES("Robert", " Pattinson");
+INSERT actores(nombre, apellido) VALUES("Robert", "Downey");
+INSERT actores(nombre, apellido) VALUES("Gwyneth", "Paltrow");
+INSERT actores(nombre, apellido) VALUES("Chris", "Evans");
+INSERT actores(nombre, apellido) VALUES("Scarlett", "Johansson");
+INSERT actores(nombre, apellido) VALUES("Ryan", "Reynolds");
+INSERT actores(nombre, apellido) VALUES("Morena", "Baccarin");
+INSERT actores(nombre, apellido) VALUES("Tom", "Cruise");
+INSERT actores(nombre, apellido) VALUES("Kelly", "McGillis");
+INSERT actores(nombre, apellido) VALUES("Tom", "Cruise");
+INSERT actores(nombre, apellido) VALUES("Henry", "Cavill");
+
+-- Directores
+INSERT directores(nombre) VALUES("James Cameron");
+INSERT directores(nombre) VALUES("Gore Verbinski");
+INSERT directores(nombre) VALUES("Joachim Rønning");
+INSERT directores(nombre) VALUES("Rob Cohen");
+INSERT directores(nombre) VALUES("Louis Leterrier");
+INSERT directores(nombre) VALUES("Catherine Hardwicke");
+INSERT directores(nombre) VALUES("Bill Condon");
+INSERT directores(nombre) VALUES("Shane Black");
+INSERT directores(nombre) VALUES("Anthony y Joe Russo");
+INSERT directores(nombre) VALUES("Tim Miller");
+INSERT directores(nombre) VALUES("Shawn Levy");
+INSERT directores(nombre) VALUES("Tony Scott");
+INSERT directores(nombre) VALUES("Brian De Palma");
+INSERT directores(nombre) VALUES("Christopher McQuarrie");
+
+-- Insert peliculas actores
+-- Titanic
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (1, 1); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (1, 2); 
+-- La maldición de la Perla Negra
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (2, 3); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (2, 4);
+-- Piratas del Caribe: La venganza de Salazar
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (3, 3);
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (3, 4); 
+-- Fast & Furious: Vin Diesel
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (4, 5); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (4, 6); 
+-- Fast X
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (5, 5); 
+ -- Crepúsculo
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (6, 7);  
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (6, 8); 
+-- La Saga Crepúsculo: Amanecer
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (7, 7); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (7, 8); 
+-- Iron Man 3
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (8, 9); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (8, 10); 
+-- Capitán América
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (9, 11); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (9, 12);  
+-- Deadpool
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (10, 13); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (10, 14); 
+-- Deadpool 3
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (11, 13); 
+-- Top Gun
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (12, 15); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (12, 16); 
+-- Misión: Imposible
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (13, 15); 
+INSERT INTO peliculas_actores (id_pelicula, id_actor) VALUES (14, 15); 
+
+-- Relación entre directores y 
+-- Titanic
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (1, 1); 
+-- La maldición de la Perla Negra
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (2, 2); 
+-- Piratas del Caribe: La venganza de Salazar
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (3, 3); 
+-- Fast & Furious
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (4, 4);
+-- Fast X
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (5, 5);
+-- Crepúsculo
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (6, 6); 
+-- La Saga Crepúsculo: Amanecer - Parte 2
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (7, 7); 
+-- Iron Man 3
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (8, 8); 
+-- Capitán América: El Soldado de Invierno
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (9, 9); 
+-- Deadpool
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (10, 10); 
+-- Deadpool 3
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (11, 11);
+-- Top Gun
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (12, 12); 
+-- Misión: Imposible
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (13, 13); 
+--  Misión: Imposible - Fallout
+INSERT INTO directores_peliculas (id_director, id_pelicula) VALUES (14, 14); 
+
+
+
+
+
+
